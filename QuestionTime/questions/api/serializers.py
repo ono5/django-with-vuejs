fbe3da56a1cf7525759f8c35e5487c558cd6a834
ono5 <kuroneko_head_lord@yahoo.co.jp>
2019-10-27 12:13:53 +0900

    feat: Added Like and Detail endpoint

--- QuestionTime/questions/api/serializers.py
+++ QuestionTime/questions/api/serializers.py
@@ -6,7 +6,7 @@ from questions.models import Answer, Question
 class AnswerSerializer(serializers.ModelSerializer):
     author = serializers.StringRelatedField(read_only=True)
     created_at = serializers.SerializerMethodField()
-    liked_count = serializers.SerializerMethodField()
+    likes_count = serializers.SerializerMethodField()
     user_has_voted = serializers.SerializerMethodField()
 
     class Meta:
@@ -16,7 +16,7 @@ class AnswerSerializer(serializers.ModelSerializer):
     def get_created_at(self, instance):
         return instance.created_at.strftime('%B %d, %Y')
 
-    def get_liked_count(self, instance):
+    def get_likes_count(self, instance):
         return instance.voters.count()
 
     def get_user_has_voted(self, instance):
--- QuestionTime/questions/api/urls.py
+++ QuestionTime/questions/api/urls.py
@@ -13,5 +13,11 @@ urlpatterns = [
          name='answer-answer'),
     path('questions/<slug:slug>/answer/',
          qv.AnswerCreateAPIView.as_view(),
-         name='create-answer'),
+         name='answer-create'),
+    path('answers/<int:pk>/',
+         qv.AnswerRUDAPIView.as_view(),
+         name='answer-detail'),
+    path('answers/<int:pk>/like/',
+         qv.AnswerLikeAPIView.as_view(),
+         name='answer-like'),
 ]
