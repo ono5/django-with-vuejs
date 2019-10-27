0cd700354c77910f939e3943f30b035ae8dc2356
ono5 <kuroneko_head_lord@yahoo.co.jp>
2019-10-27 18:27:48 +0900

    feat: Added Vue Router and Single File Components

--- QuestionTime/frontend/src/router/index.js
+++ QuestionTime/frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Example from "../views/Example.vue";
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes = [
     path: "/",
     name: "home",
     component: Home
+  },
+  {
+    path: "/example",
+    name: "example",
+    component: Example
   },
   {
     path: "/about",
@@ -23,7 +29,8 @@ const routes = [
 
 const router = new VueRouter({
   mode: "history",
-  base: process.env.BASE_URL,
+  // localhost:8000/http:/0.0.0.0:8000/aboutになってしまう
+  // base: process.env.BASE_URL,
   routes
 });
 
